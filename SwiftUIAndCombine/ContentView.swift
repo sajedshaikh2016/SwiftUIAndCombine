f3515d7cc13ecd245bf32634c14be168677d3969
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-06-02 12:37:04 +0530

    Initial Commit

---
+++ SwiftUIAndCombine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIAndCombine
+//
+//  Created by Sajed Shaikh on 02/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAndCombine/SwiftUIAndCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAndCombineApp.swift
+//  SwiftUIAndCombine
+//
+//  Created by Sajed Shaikh on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAndCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
